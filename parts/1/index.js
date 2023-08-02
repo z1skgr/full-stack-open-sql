ef25f227be1f52f50b0ea8c5c7519e35c1b1d797
z1skgr <z1skgr@yahoo.gr>
2023-08-02 11:21:31 +0300

    Exer 13.4
    13.4 Transform your application into a web application that supports the following operations
    
    GET api/blogs (list all blogs)
    POST api/blogs (add a new blog)
    DELETE api/blogs/:id (delete a blog)

--- parts/1/index.js
+++ parts/1/index.js
@@ -1,21 +1,72 @@
 require('dotenv').config()
-const { Sequelize, QueryTypes} = require('sequelize')
+const { Sequelize, Model, DataTypes} = require('sequelize')
 
-
+const express = require('express');
+const app = express();
 const sequelize = new Sequelize(process.env.DATABASE_URL)
 
-
-const printBlogs = async () => {
-  try {
-    await sequelize.authenticate()
-    const blogs = await sequelize.query('SELECT * FROM blogs', { type: QueryTypes.SELECT });
-
-    blogs.forEach((blog) => {
-      console.log(`${blog.author}: '${blog.title}', ${blog.likes} likes`);
-    });
-    sequelize.close();
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
+class Blog extends Model {}
+Blog.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  author : {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  url : {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  title  : {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  likes  : {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    defaultValue: 0
   }
-}
-printBlogs();
+  
+}, {
+  sequelize,
+  underscored: true,
+  timestamps: false,
+  modelName: 'blog'
+})
+
+Blog.sync();  
+
+app.get('/api/blogs', async (req, res) => {
+  const blogs = await Blog.findAll()
+  res.json(blogs)
+})
+
+app.post('/api/blogs', async (req, res) => {
+  try{
+    const blog = await Blog.create(req.body)
+    return res.json(blog)
+  }catch{
+    return res.status(400).json({ error })
+  } 
+})
+
+app.delete('/api/blogs/:id', async (req, res) => {
+  const note = await Blog.destroy({
+    where: {
+      id: req.params.id
+    }
+  });
+  if (!note) {
+    res.status(404).end()
+  }
+  res.status(204).end()
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
+
