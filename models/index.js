96c6df5fb679ef1654ff5eee54e63436ca202dfe
z1skgr <z1skgr@yahoo.gr>
2023-08-08 14:35:31 +0300

    refactor

--- models/index.js
+++ models/index.js
@@ -1,13 +1,10 @@
 const Blog = require('./blog');
 const User = require('./user');
-const ReadingList = require('./reading_list');
 
 User.hasMany(Blog)
 Blog.belongsTo(User)
 
-User.belongsToMany(Blog, { through: ReadingList });
-Blog.belongsToMany(User, { through: ReadingList });
 
 module.exports = {
-  Blog, User, ReadingList,
+  Blog, User
 };
