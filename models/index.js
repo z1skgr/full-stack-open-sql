a17e37bb2e152bf78d8e5206f2dc7cf4a30df8b4
z1skgr <z1skgr@yahoo.gr>
2023-08-08 10:21:30 +0300

    13.19
    13.19 Give users the ability to add blogs on the system to a reading list. When added to the reading list, the blog should be in the unread state. The blog can later be marked as read. Implement the reading list using a connection table. Make database changes using migrations.
    
    In this task, adding to a reading list and displaying the list need not be successful other than directly using the database.

--- models/index.js
+++ models/index.js
@@ -1,10 +1,13 @@
 const Blog = require('./blog');
 const User = require('./user');
+const ReadingList = require('./reading_list');
 
 User.hasMany(Blog)
 Blog.belongsTo(User)
 
+User.belongsToMany(Blog, { through: ReadingList });
+Blog.belongsToMany(User, { through: ReadingList });
 
 module.exports = {
-  Blog, User
+  Blog, User, ReadingList,
 };
